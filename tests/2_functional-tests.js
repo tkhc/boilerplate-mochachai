ef26c4b9555773b5aee385bfa7d91591128f2c4f
Terrence Chan <terrence_c@ymail.com>
2021-02-02 12:06:23 -0600

    completed

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,8 +14,8 @@ suite("Functional Tests", function () {
         .request(server)
         .get("/hello")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello Guest");
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello Guest");
           done();
         });
     });
@@ -25,8 +25,8 @@ suite("Functional Tests", function () {
         .request(server)
         .get("/hello?name=xy_z")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello xy_z");
+          assert.equal(res.status, 200);
+          assert.equal(res.text, "hello xy_z");
           done();
         });
     });
@@ -35,40 +35,76 @@ suite("Functional Tests", function () {
       chai
         .request(server)
         .put("/travellers")
-
+        .send({ surname: 'Colombo' })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200, 'response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be JSON');
+          assert.equal(
+            res.body.name,
+            'Cristoforo',
+            'res.body.name should be "Christoforo"'
+          );
+          assert.equal(
+            res.body.surname,
+            'Colombo',
+            'res.body.surname should be "Colombo"'
+          );
           done();
         });
     });
     // #4
     test('send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
+      chai
+        .request(server)
+        .put("/travellers")
+        .send({ surname:'da Verrazzano' })
+        .end(function (err, res) {
+          assert.equal(res.status, 200, 'response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be JSON');
+          assert.equal(
+            res.body.name,
+            'Giovanni',
+            'res.body.name should be "Giovanni"'
+          );
+          assert.equal(
+            res.body.surname,
+            'da Verrazzano',
+            'res.body.surname should be "da Verrazzano"'
+          );
       done();
     });
   });
 });
 
 const Browser = require("zombie");
-
+//Browser.site = 'https://boilerplate-mochachai.tkhc.repl.co';
+Browser.localhost('example.com', process.env.PORT || 3000);
 suite("Functional Tests with Zombie.js", function () {
-
+const browser = new Browser();
+suiteSetup(function(done) {
+  return browser.visit('/', done);
+});
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
       browser.fill("surname", "Colombo").pressButton("submit", function () {
-        assert.fail();
-
+        browser.assert.success();
+        browser.assert.text('span#name', 'Cristoforo');
+        browser.assert.text('span#surname', 'Colombo');
+        browser.assert.element('span#dates', 1);
         done();
       });
     });
     // #6
     test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill("surname", "Vespucci").pressButton("submit", function () {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.element('span#dates', 1);
+        done();
     });
   });
 });
+});
+})
